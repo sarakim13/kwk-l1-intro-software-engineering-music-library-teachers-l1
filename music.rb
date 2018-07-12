97ad9aa19581a28d5d27f4f35379f8880efd7720
Sara Kim <sarakim0903@gmail.com>
2018-07-12 18:32:12 +0000

    automatically backed up by learn

--- music.rb
+++ music.rb
@@ -21,12 +21,12 @@ end
 #This method should take two arguments, the music library hash, and the artist. Remember, keys in hashes are symbols, so you'll need to expect the method to accept the artist as a symbol.
 def delete_artist(lib, artist)
   lib.delete(artist)
-  lib
+  return lib
 end
 
 #4. Define a method `add_song_and_artist`. This method should add a new artist and song (key value pair) to the hash and return the updated hash.
 # This method should accept three arguments, the library hash, the artist, and the song.
 def add_song_and_artist(hash, artist, song)
   hash[artist] = song
-  hash
+  return hash
 end
